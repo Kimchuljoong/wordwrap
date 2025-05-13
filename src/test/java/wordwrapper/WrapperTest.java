456eacc51694e1387391797801dc1599cb6aaaa3
Kimchuljoong <wnmh123@gmail.com>
2025-05-13 23:29:44 +0900

    test stucked

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -2,8 +2,19 @@ package wordwrapper;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class WrapperTest {
+
     @Test
-    public void nothing() {
+    public void should_wrap() {
+        assertThat(wrap("word word", 4), is("word\nword"));
+        assertThat(wrap("a dog", 5), is("a dog"));
+        assertThat(wrap("a dog with a bone", 6), is("a dog\nwith a\nbone"));
+    }
+
+    private String wrap(String s, int width) {
+        return s.length() > width ? s.replaceAll(" ", "\n") : s;
     }
 }
